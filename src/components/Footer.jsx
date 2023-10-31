2b6ac0b83480cfbec00318a3900a4b71c171ca59
brunoguima7 <brunodecamargoguimaraes@gmail.com>
2023-10-31 15:29:00 -0300

    deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styles/global.css"
 export default function Footer() {
     return (
         <div className="footer">
-            <span id="date">2023 -</span><span id="footer-text">MEAN GIRLS PARTY.</span>  <span id="contact-text">Sophia Arantes 11 99186-0052 <br /> Isabelle Guimarães 11 92000-2726</span>
+            <span id="date">2023 -</span><span id="footer-text">MEAN GIRLS PARTY.</span>  <span id="contact-text">Sophia Arantes 11 99186-0052 <br /> Belle Guimarães 11 92000-2726</span>
         </div>
     )
 }
